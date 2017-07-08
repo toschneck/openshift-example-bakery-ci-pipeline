939d647e8e82dc92a0c85c3893b139bc88c1ae4b
Tobias Schneck <tobias.schneck@consol.de>
2017-07-08 15:18:18 +0200

    add ENV_PROFILE (consol or minishift) to CI config

--- openshift/infrastructur/create-infrastrutur.sh
+++ openshift/infrastructur/create-infrastrutur.sh
@@ -14,7 +14,9 @@ if [[ $1 =~ delete ]]; then
     fi
     exit 0
 fi
+ENV_PROFILE="${ENV_PROFILE:-consol}"
 
 echo "============= CREATE INFRASTRUCTUR =================="
+echo "ENV_PROFILE=${ENV_PROFILE}"
 oc process -f $FOLDER/jenkins.persistent.yml | oc apply -f - \
-    && oc process -f $FOLDER/jenkins.yml | oc apply -f -
+    && oc process -v ENV_PROFILE=${ENV_PROFILE} -f $FOLDER/jenkins.yml | oc apply -f -
