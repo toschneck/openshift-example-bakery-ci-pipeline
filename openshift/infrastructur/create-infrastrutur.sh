1845153da043254e96b364efda8706aeb9cb9b0f
Tobias Schneck <tobias.schneck@consol.de>
2017-07-09 17:57:28 +0200

    add configurable image registry to jenkins

--- openshift/infrastructur/create-infrastrutur.sh
+++ openshift/infrastructur/create-infrastrutur.sh
@@ -8,9 +8,19 @@ if [ -z  $NEXUS_HOST ]; then
     NEXUS_HOST="nexus-nexus.paas.osp.consol.de"
 fi
 
+if [ -z $IMAGE_REG ]; then
+    #local openshift
+#    IMAGE_PREFIX='172.30.1.1:5000'
+    #consol openshift
+    IMAGE_REG='172.30.1.1:5000'
+fi
+
 if [[ $1 =~ delete ]]; then
     echo "============= DELETE INFRASTRUCTUR =================="
-    oc process -v NEXUS_HOST=${NEXUS_HOST} -f $FOLDER/jenkins.yml | oc delete -f -
+    oc process -f $FOLDER/jenkins.yml \
+        -v NEXUS_HOST=${NEXUS_HOST} \
+        -v IMAGE_REG=${IMAGE_REG} \
+        | oc delete -f -
     echo "tried to delete not persistent content"
     if [[ $1 =~ delete-all ]]; then
         oc process -f $FOLDER/jenkins.persistent.yml | oc delete -f -
@@ -21,6 +31,10 @@ fi
 
 echo "============= CREATE INFRASTRUCTUR =================="
 echo "NEXUS_HOST=${NEXUS_HOST}"
+echo "IMAGE_REG=${IMAGE_REG}"
 
 oc process -f $FOLDER/jenkins.persistent.yml | oc apply -f - \
-    && oc process -v NEXUS_HOST=${NEXUS_HOST} -f $FOLDER/jenkins.yml | oc apply -f -
+    && oc process -f $FOLDER/jenkins.yml \
+            -v NEXUS_HOST=${NEXUS_HOST} \
+            -v IMAGE_REG=${IMAGE_REG} \
+            | oc apply -f -
