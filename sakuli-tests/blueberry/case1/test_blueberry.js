892698b499ef55b9bf05720767d9b62da4750a91
Tobias Schneck <tobias.schneck@consol.de>
2017-07-17 15:38:23 +0200

    Revert: add error to blueberry test

--- sakuli-tests/blueberry/case1/test_blueberry.js
+++ sakuli-tests/blueberry/case1/test_blueberry.js
@@ -36,7 +36,7 @@ try {
 
 
     _navigateTo($bakeryURL);
-    visibleHighlight(_paragraph("Place XXXXXXXX orders:"));
+    visibleHighlight(_paragraph("Place new orders:"));
     adjustAmount();
     testCase.endOfStep("move amount slider", 40);
 
